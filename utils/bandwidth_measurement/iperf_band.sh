1ee43951decd3ddccbcffcc4928e8ff2174cdbfd
Neel Patel <n869p538@castor.ittc.ku.edu>
2022-03-19 22:17:22 -0500

    iperf bandwidth test

--- utils/bandwidth_measurement/iperf_band.sh
+++ utils/bandwidth_measurement/iperf_band.sh
@@ -7,12 +7,19 @@ export WRK_ROOT=/home/n869p538/wrk_offloadenginesupport
 wrk_output=/home/n869p538/wrk_offloadenginesupport/wrk_files
 
 #establish servers on remote host
-
-outfile=${wrk_output}/tcp_con_band.txt
-echo -n "" >  $outfile.txt
+ssh n869p538@pollux.ittc.ku.edu & <<"FI" 
 for i in `seq 2 12`;
 do 
-	taskset -c $i /usr/bin/iperf -c ${serv_48}.$i.${serv_24} -B ${cli_48}.$i.${cli_24} | grep GBytes >> $outfile &
+	taskset -c $i /usr/bin/iperf -s -B 192.168.$i.2 & 
 done
+FI
 
-cat $outfile
+outfile=${wrk_output}/tcp_con_band.txt
+echo -n "" >  $outfile
+for i in `seq 2 12`;
+do 
+	taskset -c $(($i - 1)),$(($i + 19)) /usr/bin/iperf -P2 -t 60 -c ${serv_48}.$i.${serv_24} -B ${cli_48}.$i.${cli_24} | grep GBytes >> $outfile &
+done
+wait
+
+awk '{sum+=$7} END {print sum}' $outfile
