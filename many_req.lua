fd5f2e839e49a5bf02093ba9a181e9d724bd902a
Neel Patel <n869p538@castor.ittc.ku.edu>
2022-11-08 13:58:00 -0600

    needed lua

---
+++ many_req.lua
@@ -0,0 +1,8 @@
+-- init random
+math.randomseed(os.time())-- the request function that will run at each request
+request = function() 
+	-- url_path = "/UCFile_1K_" .. math.random(0,9983.0) .. ".txt" -- if we want to print the path generated
+	url_path = "/UCFile_1K_" .. math.random(0,399) .. ".txt" -- if we want to print the path generated
+	-- url_path = "/UCFile_16K_" .. math.random(0,99) .. ".txt" -- if we want to print the path generated
+   return wrk.format("GET", url_path)
+end
